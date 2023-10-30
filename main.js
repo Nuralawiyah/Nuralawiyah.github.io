8e2bf940f16ccb340c28e87fd3744fe974cc8b30
Nuralawiyah <149327313+Nuralawiyah@users.noreply.github.com>
2023-10-30 09:34:02 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.text', {
+    strings: ['<i>Student Of</i> UIN SUSKA RIAU,', 'Program <i>Information System.</i> '],
+    typeSpeed: 50,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+  });
